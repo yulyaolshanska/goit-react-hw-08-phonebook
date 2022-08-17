aae613ad7311fac04ae48874469b8aaa745f51aa
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-08-18 00:00:33 +0300

    Auth Operations
    Register

---
+++ src/redux/auth/authOperations.js
@@ -0,0 +1,17 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+
+const register = createAsyncThunk('auth/register', async credentials => {
+  try {
+    const { data } = await axios.post('/users/signup', credentials);
+    return data;
+  } catch (error) {}
+});
+
+const authOperations = {
+  register,
+};
+
+export default authOperations;
