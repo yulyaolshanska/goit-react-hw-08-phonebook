42f44a54bf8f09d9c21b3adc914f8a3ccf08d97c
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-08-17 20:52:37 +0300

    Разметка и стилизация Navigation

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,25 @@
+import { NavLink } from 'react-router-dom';
+import styles from './Navigation.module.css';
+
+export const Navigation = () => {
+  return (
+    <nav>
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive ? styles.activeLink : styles.link
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/contacts"
+        className={({ isActive }) =>
+          isActive ? styles.activeLink : styles.link
+        }
+      >
+        Contacts
+      </NavLink>
+    </nav>
+  );
+};
