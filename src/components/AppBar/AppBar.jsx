4b80d286181e14373def333c708b1e1c7b0fe11e
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-08-17 19:37:07 +0300

    Структура AppBar

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,4 +1,6 @@
 import { Outlet } from 'react-router-dom';
+import { Navigation } from 'components/Navigation';
+import { UserMenu } from 'components/UserMenu';
 
 export function AppBar() {
   return (
