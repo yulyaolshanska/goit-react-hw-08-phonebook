f2c80d1da1b6b2095697a5ead943d24d6a1811ef
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-08-17 23:58:57 +0300

    Добавление Auth в store
     Persist для   authSlice.reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,50 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { contactsApi } from './contactsSlice';
 import { filterReducer } from 'redux/filterSlice';
 import { setupListeners } from '@reduxjs/toolkit/query';
+import storage from 'redux-persist/lib/storage';
+
+// import { persistedReducer } from './contactsSlice';
+
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import persistReducer from 'redux-persist/es/persistReducer';
+import authSlice from './auth/authSlice';
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  blacklist: ['token'],
+};
+
+export const persistedReducer = persistReducer(
+  authPersistConfig,
+  authSlice.reducer
+);
 
 export const store = configureStore({
   reducer: {
+    auth: persistedReducer,
     [contactsApi.reducerPath]: contactsApi.reducer,
     filter: filterReducer,
   },
   middleware: getDefaultMiddleware => [
-    ...getDefaultMiddleware(),
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+
     contactsApi.middleware,
   ],
 });
 
-setupListeners(store.dispatch);
+// setupListeners(store.dispatch);
+export const persistor = persistStore(store);
