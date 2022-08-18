8b7fb4bf78f89d7b2340072a7971fc8d42353477
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-08-18 20:39:03 +0300

    Log In form
    Change and Submit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,22 @@
+import { useSelector } from 'react-redux';
+import authSelectors from 'redux/auth/authSelectors';
+
 const { NavLink } = require('react-router-dom');
 
 const Home = () => {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
   return (
     <>
       <h1>Phonebook</h1>
-      <p>Log In or Register and use Phonebook</p>
-      <NavLink to="/login">Log In</NavLink>
-      <NavLink to="/register">Register</NavLink>
+      {isLoggedIn ? (
+        <p>Welcome to Phonebook</p>
+      ) : (
+        <div>
+          <p>Log In or Register and use Phonebook</p>
+          <NavLink to="/login">Log In</NavLink>
+          <NavLink to="/register">Register</NavLink>
+        </div>
+      )}
     </>
   );
 };
--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,15 +1,57 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import authOperations from 'redux/auth/authOperations';
+
 const LogInView = () => {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'email':
+        setEmail(value);
+        break;
+      case 'password':
+        setPassword(value);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const resetForm = () => {
+    setEmail('');
+    setPassword('');
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(authOperations.logIn({ email, password }));
+    resetForm();
+  };
+
   return (
     <>
       <h1>Log In</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Email
-          <input type="email" name="email" value="email" />
+          <input
+            onChange={handleChange}
+            type="email"
+            name="email"
+            value={email}
+          />
         </label>
         <label>
           Password
-          <input type="password" name="password" value="password" />
+          <input
+            onChange={handleChange}
+            type="password"
+            name="password"
+            value={password}
+          />
         </label>
         <button type="submit">Log In</button>
       </form>
