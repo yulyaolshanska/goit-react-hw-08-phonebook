9a960b8332c9c2dec06c36b62906ff2f5e874f29
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-08-18 16:48:45 +0300

    Register fulfilled

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -14,8 +14,13 @@ const authSlice = createSlice({
   name: 'auth',
   initialState,
   extraReducers: {
-    [authOperations.register.fulfilled](state, actions) {},
+    [authOperations.register.fulfilled](state, action) {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+    },
   },
 });
 
-export default authSlice.reducer;
+// export default authSlice.reducer;
+export const authReducer = authSlice.reducer;
