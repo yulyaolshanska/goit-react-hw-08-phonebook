e36437d94e5cb2e3a63ebf77d746d57c90f25f28
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-08-19 00:17:50 +0300

    Token for Contacts

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -4,12 +4,21 @@ export const contactsApi = createApi({
   reducerPath: 'contacts',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://connections-api.herokuapp.com',
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().auth.token;
+      if (token) {
+        console.log('token', token);
+        headers.set('authorization', `Bearer ${token}`);
+      }
+      return headers;
+    },
   }),
+
   tagTypes: ['Contacts'],
 
   endpoints: builder => ({
     getContacts: builder.query({
-      query: () => `/contacts`,
+      query: () => '/contacts',
       providesTags: ['Contacts'],
     }),
 
