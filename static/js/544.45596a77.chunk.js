eb588f067ea94c213624f17de6fec7b1a43230d6
yulyaolshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-08-18 17:43:43 +0000

    Deploying to gh-pages from @ yulyaolshanska/goit-react-hw-08-phonebook@155cc8f029787dafaff7ff2742e17d3f9a60a52e 🚀

--- static/js/544.45596a77.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[544],{9544:function(e,n,r){r.r(n);var o=r(184),t=r(501).NavLink;n.default=function(){return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("h1",{children:"Phonebook"}),(0,o.jsx)("p",{children:"Log In or Register and use Phonebook"}),(0,o.jsx)(t,{to:"/login",children:"Log In"}),(0,o.jsx)(t,{to:"/register",children:"Register"})]})}}}]);
-//# sourceMappingURL=544.45596a77.chunk.js.map
