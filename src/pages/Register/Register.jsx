a570c75451952b515e171b41307ed9fe72b6beb5
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-08-18 16:47:42 +0300

    Change and Submit for  Register

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,19 +1,66 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import authOperations from 'redux/auth/authOperations';
+
 const RegisterView = () => {
+  const dispatch = useDispatch();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'email':
+        setEmail(value);
+        break;
+      case 'password':
+        setPassword(value);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const resetForm = () => {
+    setName('');
+    setEmail('');
+    setPassword('');
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(authOperations.register({ name, email, password }));
+    resetForm();
+  };
+
   return (
     <>
       <h1>Register</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Name
-          <input type="text" name="name" value="" />
+          <input onChange={handleChange} type="text" name="name" value={name} />
         </label>
         <label>
           Email
-          <input type="email" name="email" value="" />
+          <input
+            onChange={handleChange}
+            type="email"
+            name="email"
+            value={email}
+          />
         </label>
         <label>
           Password
-          <input type="password" name="password" value="" />
+          <input
+            onChange={handleChange}
+            type="password"
+            name="password"
+            value={password}
+          />
         </label>
         <button type="submit">Register</button>
       </form>
