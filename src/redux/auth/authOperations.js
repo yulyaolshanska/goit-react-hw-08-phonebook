173e2cad0e617b8eacb9cfe381de8c01ed0bcd02
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-08-18 20:40:29 +0300

    Token, Auth Operations
    1. Log In.
    2. Log Out.

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -3,18 +3,55 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
+const token = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
+};
+
 const register = createAsyncThunk('auth/register', async credentials => {
   try {
-    console.log('credentials', credentials);
+    // console.log('credentials', credentials);
     const { data } = await axios.post('/users/signup', credentials);
+    token.set(data.token);
+    console.log(data.token);
     return data;
   } catch (error) {
     console.log('error', error);
   }
 });
 
+const logIn = createAsyncThunk('auth/login', async credentials => {
+  try {
+    // console.log('credentials', credentials);
+    const { data } = await axios.post('/users/login', credentials);
+    token.set(data.token);
+    console.log(data.token);
+
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+const logOut = createAsyncThunk('auth/logout', async () => {
+  try {
+    // console.log('credentials', credentials);
+    await axios.post('/users/logout');
+    console.log('logOut');
+    token.unset();
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
 const authOperations = {
   register,
+  logIn,
+  logOut,
 };
 
 export default authOperations;
