11b35c634257893f95d8496dcc561a50eb2d403a
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-08-19 20:36:51 +0300

    Private and Public Route
    FetchingCurrentUser

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,15 @@
 // import { nanoid } from 'nanoid';
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 import { Loader } from './Loader/Loader';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import css from './App.module.css';
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import authOperations from 'redux/auth/authOperations';
+import PrivateRoute from './Routes/PrivateRoute';
+import PublicRoute from './Routes/PublicRoute';
+import authSelectors from 'redux/auth/authSelectors';
 
 const AppBar = lazy(() => import('components/AppBar/AppBar'));
 const Home = lazy(() => import('pages/Home/Home'));
@@ -13,21 +19,60 @@ const ContactsView = lazy(() => import('pages/Contacts/Contacts'));
 const NotFound = lazy(() => import('pages/NotFound/NotFound'));
 
 export function App() {
-  return (
-    <div className={css.container}>
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<AppBar />}>
-            <Route index element={<Home />} />
-            <Route path="register" element={<RegisterView />} />
-            <Route path="login" element={<LogInView />} />
-            <Route path="contacts" element={<ContactsView />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </Suspense>
+  const dispatch = useDispatch();
+  const isFetchingCurrentUser = useSelector(
+    authSelectors.getIsFetchingCurrentUser
+  );
 
-      <ToastContainer />
-    </div>
+  useEffect(() => {
+    dispatch(authOperations.fetchCurrentUser());
+  }, [dispatch]);
+
+  return (
+    !isFetchingCurrentUser && (
+      <div className={css.container}>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<AppBar />}>
+              <Route
+                index
+                element={
+                  <PublicRoute>
+                    <Home />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="register"
+                element={
+                  <PublicRoute restricted>
+                    <RegisterView />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="login"
+                element={
+                  <PublicRoute restricted>
+                    <LogInView />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="contacts"
+                element={
+                  <PrivateRoute>
+                    <ContactsView />
+                  </PrivateRoute>
+                }
+              />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </Suspense>
+
+        <ToastContainer />
+      </div>
+    )
   );
 }
