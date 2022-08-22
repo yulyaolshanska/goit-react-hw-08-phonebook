3e5cdcc41da743d18aa41994205ac21b96f57b06
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-08-22 20:42:42 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,36 @@
+import { Nav } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import authSelectors from 'redux/auth/authSelectors';
 
-const { NavLink } = require('react-router-dom');
+// const { NavLink } = require('react-router-dom');
+
+import { Container } from 'react-bootstrap';
 
 const Home = () => {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
   return (
-    <>
+    <Container className="mt-5">
       <h1>Phonebook</h1>
       {isLoggedIn ? (
         <p>Welcome to Phonebook</p>
       ) : (
         <div>
-          <p>Log In or Register and use Phonebook</p>
-          <NavLink to="/login">Log In</NavLink>
-          <NavLink to="/register">Register</NavLink>
+          <p>Log In or Register and use Phonebook.</p>
+          <Nav className="justify-content-center">
+            <Nav.Item>
+              <Nav.Link href="/login">Log In</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/register">Register</Nav.Link>
+            </Nav.Item>
+          </Nav>
         </div>
       )}
-    </>
+    </Container>
   );
 };
 
 export default Home;
+
+/* <NavLink to="/login">Log In</NavLink>
+          <NavLink to="/register">Register</NavLink> */
