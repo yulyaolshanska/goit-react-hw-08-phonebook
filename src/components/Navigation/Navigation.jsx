f9576273bca9ed44ce3a2300a8c8727779d44756
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-08-22 20:42:10 +0300

    Navbar
    Active nav link.

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,41 +1,44 @@
-import { Nav } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 // import { NavLink } from 'react-router-dom';
 import authSelectors from 'redux/auth/authSelectors';
 // import styles from './Navigation.module.css';
 
 export const Navigation = () => {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+  const location = useLocation();
 
   return (
-    // <Navbar bg="primary" variant="dark">
-
-    <Nav className="me-auto">
-      <Nav.Link href="/">Home</Nav.Link>
-      {isLoggedIn && <Nav.Link href="/contacts">Contacts</Nav.Link>}
-    </Nav>
-
-    // </Navbar>
-
-    // <nav>
-    //   <NavLink
-    //     to="/"
-    //     className={({ isActive }) =>
-    //       isActive ? styles.activeLink : styles.link
-    //     }
-    //   >
-    //     Home
-    //   </NavLink>
-    //   {isLoggedIn && (
-    //     <NavLink
-    //       to="/contacts"
-    //       className={({ isActive }) =>
-    //         isActive ? styles.activeLink : styles.link
-    //       }
-    //     >
-    //       Contacts
-    //     </NavLink>
-    //   )}
-    // </nav>
+    <Navbar bg="primary" variant="dark">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav variant="pills" activeKey={location.pathname} className="me-auto ">
+          <Nav.Link href="/">Home</Nav.Link>
+          {isLoggedIn && <Nav.Link href="/contacts">Contacts</Nav.Link>}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
+
+// <nav>
+//   <NavLink
+//     to="/"
+//     className={({ isActive }) =>
+//       isActive ? activeLink : link
+//     }
+//   >
+//     Home
+//   </NavLink>
+//   {isLoggedIn && (
+//     <NavLink
+//       to="/contacts"
+//       className={({ isActive }) =>
+//         isActive ? styles.activeLink : styles.link
+//       }
+//     >
+//       Contacts
+//     </NavLink>
+//   )}
+// </nav>
