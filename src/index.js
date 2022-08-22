06aaf367c8995fe13dda3c3e78748559338814ec
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-08-22 20:47:20 +0300

    fix BrowserRouter

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store} basename="goit-react-hw-08-phonebook">
+    <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="goit-react-hw-08-phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
