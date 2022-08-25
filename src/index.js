42eb58b4f1613bd825d59e1f4a181c82ea31cef8
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-08-25 23:47:52 +0300

    Fix toastify

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { persistor, store } from './redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
